3ff1988fb84d48f550539ce626a07eea105cd96c
Cristian Mera <cristianlmera@gmail.com>
2021-11-28 14:20:32 -0500

    primer version proyecto libreria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LibroComponent } from './libros/libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibroComponent
   ],
   imports: [
     BrowserModule
